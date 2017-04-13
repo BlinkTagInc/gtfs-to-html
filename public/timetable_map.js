27444d1a13b817f39446942e2d561d44d990bf3c
Brendan Nee <brendan@blinktag.com>
2017-04-13 01:57:42 -0700

    Use mapbox GL for timetable map

---
+++ public/timetable_map.js
@@ -0,0 +1,66 @@
+function createMap(id, stops, shapes) {
+  var bounds = new mapboxgl.LngLatBounds();
+  var map = new mapboxgl.Map({
+    container: 'map_' + id,
+    style: 'mapbox://styles/mapbox/streets-v9',
+    center: [stops[0].stop_lon, stops[0].stop_lat],
+    zoom: 12
+  });
+
+  map.scrollZoom.disable();
+  map.addControl(new mapboxgl.NavigationControl());
+
+  function createMarker() {
+    var el = document.createElement('div');
+    el.className = 'marker';
+    return el;
+  }
+
+  map.on('load', function () {
+    stops.forEach(function(stop) {
+      var coord = [stop.stop_lon, stop.stop_lat];
+
+      var popup = new mapboxgl.Popup({offset: 15}).setHTML('<b>' + stop.stop_name + '</b>');
+      var marker = new mapboxgl.Marker(createMarker(), {offset: [-7, -7]})
+        .setLngLat(coord)
+        .setPopup(popup)
+        .addTo(map);
+
+      bounds.extend(coord);
+    });
+
+    map.fitBounds(bounds, {
+      padding: 20
+    });
+
+    if (!shapes || !shapes.length) {
+      shapes = [stops.map(function(stop) {
+        return [stop.stop_lon, stop.stop_lat];
+      })];
+    }
+
+    map.addLayer({
+      id: 'shape',
+      type: 'line',
+      source: {
+        type: 'geojson',
+        data: {
+          type: 'Feature',
+          properties: {},
+          geometry: {
+            type: 'MultiLineString',
+            coordinates: shapes
+          }
+        }
+      },
+      layout: {
+        'line-join': 'round',
+        'line-cap': 'round'
+      },
+      paint: {
+        'line-color': '#4171ff',
+        'line-width': 4
+      }
+    });
+  });
+}
