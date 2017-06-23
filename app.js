f70365bca4fce5982227bd80105e44d55a64b133
Brendan Nee <brendan@blinktag.com>
2017-06-23 02:05:45 -0700

    Remove body-parser

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const path = require('path');
 
-const bodyParser = require('body-parser');
 const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
@@ -31,8 +30,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
