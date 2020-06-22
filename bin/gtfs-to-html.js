e5a3e298303ab02bd26f1646a7607beb2a0a8fab
Brendan Nee <brendan@blinktag.com>
2020-06-22 15:13:31 -0700

    Better error handling

--- bin/gtfs-to-html.js
+++ bin/gtfs-to-html.js
@@ -31,12 +31,6 @@ const argv = require('yargs').usage('Usage: $0 --config ./config.json')
 const logUtils = require('../lib/log-utils');
 const gtfsToHtml = require('..');
 
-function handleError(error) {
-  const text = error || 'Unknown Error';
-  process.stdout.write(`\n${logUtils.formatError(text)}\n`);
-  throw error;
-}
-
 const getConfig = async () => {
   const data = await fs.readFile(resolve(argv.configPath), 'utf8').catch(() => {
     throw new Error(`Cannot find configuration file at \`${argv.configPath}\`. Use config-sample.json as a starting point, pass --configPath option`);
@@ -62,11 +56,14 @@ const getConfig = async () => {
 
 getConfig()
   .then(async config => {
-    mongoose.Promise = global.Promise;
     mongoose.connect(config.mongoUrl, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
 
     await gtfsToHtml(config);
 
     process.exit();
   })
-  .catch(handleError);
+  .catch((error) => {
+    process.stdout.write('\n');
+    console.error(error);
+    process.exit();
+  });
