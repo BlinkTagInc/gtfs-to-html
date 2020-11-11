b749bd0b5edb606134eef6310374cc7c97b01deb
Brendan Nee <brendan@blinktag.com>
2020-11-10 17:41:59 -0800

    Update sidebars.js

--- www/sidebars.js
+++ www/sidebars.js
@@ -2,7 +2,7 @@ module.exports = {
   someSidebar: {
     'Getting Started': ['introduction', 'quick-start', 'current-usage'],
     Configuration: ['configuration', 'custom-templates'],
-    'Additional GTFS Files': ['additional-files', 'timetables', 'timetable-stop-order', 'timetable-pages', 'stop-attributes'],
+    'Additional GTFS Files': ['additional-files', 'timetables', 'timetable-stop-order', 'timetable-pages', 'stop-attributes', 'timetable-notes', 'timetable-notes-references'],
     Tips: ['previewing-html-output', 'processing-large-gtfs', 'reviewing-changes'],
     'Related Libraries': ['related-libraries']
   }
