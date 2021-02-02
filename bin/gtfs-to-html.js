ce795563566e9fa7d206f3edd5ab206a2adb30f8
Brendan Nee <brendan@blinktag.com>
2021-02-02 12:19:22 -0800

    Lints

--- bin/gtfs-to-html.js
+++ bin/gtfs-to-html.js
@@ -27,10 +27,10 @@ const fileUtils = require('../lib/file-utils');
 const logUtils = require('../lib/log-utils');
 const gtfsToHtml = require('..');
 
-const handleError = err => {
-  const text = err || 'Unknown Error';
+const handleError = error => {
+  const text = error || 'Unknown Error';
   process.stdout.write(`\n${logUtils.formatError(text)}\n`);
-  console.error(err);
+  console.error(error);
   process.exit(1);
 };
 
