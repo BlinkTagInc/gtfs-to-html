3a2429f5a7556c15eb81a492f3236769298c8a95
Brendan Nee <brendan@blinktag.com>
2024-05-14 15:54:53 -0700

    Improved error formatting

--- bin/gtfs-to-html.js
+++ bin/gtfs-to-html.js
@@ -2,10 +2,13 @@
 
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
+import PrettyError from 'pretty-error';
 
 import { getConfig } from '../lib/file-utils.js';
 import { formatError } from '../lib/log-utils.js';
 import gtfsToHtml from '../index.js';
+
+const pe = new PrettyError();
 
 const { argv } = yargs(hideBin(process.argv))
   .usage('Usage: $0 --configPath ./config.json')
@@ -32,7 +35,7 @@ const { argv } = yargs(hideBin(process.argv))
 const handleError = (error) => {
   const text = error || 'Unknown Error';
   process.stdout.write(`\n${formatError(text)}\n`);
-  console.error(error);
+  console.error(pe.render(error));
   process.exit(1);
 };
 
